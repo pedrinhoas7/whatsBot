5f8f2c82b2f5c7b0a7c1d1b424c01232001af249
Pedro Henrique de Moraes Batista <pedro.moraes@vsoft.com.br>
2024-08-16 21:53:48 -0300

    add deploy config

--- src/index.js
+++ src/index.js
@@ -1,63 +1,41 @@
 const qrcode = require('qrcode-terminal');
-const lembrete = require('./models/lembrete')
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+// Crie uma nova instância do cliente
+const client = new Client({
+    puppeteer: {
+        headless: false,
+        args: ['--no-sandbox'],
+        timeout: 600000 // Aumenta o timeout para 60 segundos
+    }
 });
 
+
+// Gera o QR Code para autenticação
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+// Exibe uma mensagem quando o cliente estiver autenticado
 client.on('ready', () => {
-    console.log('Conectado no bot whatsapp');
+    console.log('Cliente está pronto!');
 });
 
+// Responde a mensagens recebidas
+client.on('message', message => {
+    console.log(`Mensagem recebida: ${message.body}`);
+    console.log(`Mensagem recebida: ${message}`);
+    if (message.body.toLowerCase().includes('consulta')) {
+        message.reply('Para agendar uma consulta, por favor, ligue para (XX) XXXXX-XXXX ou acesse nosso site.');
+    } else if (message.body.toLowerCase().includes('horário')) {
+        message.reply('Nossos horários de atendimento são de segunda a sexta, das 9h às 18h.');
+    } else if (message.body.toLowerCase().includes('falar com')) {
+        message.reply('Em breve, um fonoaudiólogo entrará em contato com você.');
+    } else if (message.body.toLowerCase().includes('olá')){
+        message.reply('Olá! Como posso ajudá-lo? Você pode perguntar sobre consulta, horário de atendimento ou pedir para falar com um fonoaudiólogo.');
+    }
+    
+});
+
+// Inicia o cliente
 client.initialize();
-
-client.on('message', message => {
-	console.log(message.body);
-});
-
-client.on('message', message => {
-	if(message.body === '/start') {
-        console.log(message)
-		client.sendMessage(message.from, `Bem vindo \u{1F60D}`);
-        client.sendMessage(message.from, `O BOT foi programado para reconhecer um comando\n
-        \u{1F4CC}Lembrete: /lembrete`);
-	}
-});
-
-client.on('message', message => {
-	if(message.body === '/lembrete') {
-        console.log(message)
-        client.sendMessage(message.from, `O BOT foi programado para reconhecer um comando\n
-        \nfunções para lembrete \n
-        \n/allLembrete
-        \n/getLembreteId id
-        \n/deleteLembreteId id
-        \n/updateLembrete id, nome, descrição,hora
-        \n/createLembrete nome, descrição,hora`);
-	}
-});
-
-client.on('message',async message => {
-	if(message.body === '/allLembretes') {
-        lembretes = await lembrete.findAll();
-        console.log(message, lembretes)
-		client.sendMessage(message.from, `Bem vindo \u{1F60D}`);
-        client.sendMessage(message.from, `O BOT foi programado para reconhecer um comando\n
-        \u{1F4CC}Lembrete: /lembrete`);
-	}
-});
-
-client.on('message',async message => {
-	if(message.body.substring(0,12) === 'Bom dia amor') {
-		client.sendMessage(message.from, `Bom Diaa meu amor`);
-
-	}
-});
-
-/* client.on('message', message => {
-	if(message.body === '!ping') {
-		message.reply('pong');
-	}
-}); */
